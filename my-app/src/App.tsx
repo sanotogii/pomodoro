307d0b4d00acfbd7f99e48278b1513687526d8fd
sanotogii <nasaereh@gmail.com>
2024-11-05 10:30:17 +0100

    Add Pomodoro Timer component and main App structure

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Pomodoro from './pomodoro'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen w-screen p-4">
+      <div className="w-[260px]">
+        <Pomodoro />
+      </div>
+    </div>
+  )
+}
+
+export default App
